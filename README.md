3226b7ad1bf7612d1f9485f8a5ccc1e148b23ff5
Jeric O. Newby <tech@newbyan.com>
2019-07-31 17:21:17 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## technicalbradah's personal Dist::Zilla minting setup
+
+To use this you better know [Dist::Zilla](https://metacpan.org/pod/Dist::Zilla). Also see the docs at [dzil.org](http://dzil.org).
---
+++ profiles/personal/Module.pm
@@ -0,0 +1,39 @@
+{{
+    # Gen TT tokens to be munged later by MungeFile during dzil build step.
+    $c = sub { return $_[0] =~ s/^(\w+)$/{{\$$1}}/r };
+
+    '';
+}}package {{$name}};
+# ABSTRACT: Add an abstract here...
+
+use Newby::Standard::Imports;
+
+1;
+
+__END__
+
+=head1 SYNOPSIS
+
+    use {{$name}};
+
+=head1 DESCRIPTION
+
+[Describe this module]
+
+=head1 USAGE
+
+[Describe this module's usage]
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<{{$c->('homepage_url')}}>.
+
+=head1 SOURCE
+
+Source repository is at L<{{$c->('repository_url')}}>.
+
+=head1 BUGS
+
+If you have any suggestions for improvements, or find any bugs, please report them to L<{{$c->('issues_url')}}>.  I will be notified, and then you'll automatically be notified of progress on your bug as I make changes.
+
+=cut
